cb4d2390611a0388622eced00e8ea6995457cc16
tolgacn <156241371+tolgacn@users.noreply.github.com>
2024-01-18 15:52:25 +0300

    Create  script.sh

---
+++ #!/bin/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+permission=$1
+username=$2
+groups=$3
+file=$4
+ad=$5
+soyad=$6
+numaram=$7
+
+
+echo "file: $4, permission: $1, user: $2, group: $3
+echo "Merhaba adım $isim, soyadım ise $soyisim, Okul numaram ise $numaram"
